8b291b288e1f1d1da7f79ea233acb4086a68e8c2
rhendriks <remi.hendriks@utwente.nl>
2023-03-16 15:55:01 +0100

    Added inbound/outbound methods for TCP scanning

--- src/client/outbound.rs
+++ src/client/outbound.rs
@@ -6,7 +6,7 @@ use std::num::NonZeroU32;
 use std::thread;
 use std::time::{Duration, SystemTime, UNIX_EPOCH};
 
-use crate::net::{ICMP4Packet, UDPPacket};
+use crate::net::{ICMP4Packet, TCPPacket, UDPPacket};
 use std::net::{Ipv4Addr, SocketAddr};
 use std::sync::Arc;
 use tokio::sync::oneshot::Receiver;
@@ -98,7 +98,7 @@ pub fn perform_udp(dest_addresses: Vec<u32>, socket: Arc<Socket>, mut rx_f: Rece
                 //     .unwrap()
                 //     .as_nanos() as u64;
 
-                // // Create udp payload // TODO UDP payload does not echo
+                // // Create udp payload // TODO encode payload into the DNS request fields that will be returned
                 // let payload = PingPayload {
                 //     task_id,
                 //     transmit_time,
@@ -114,7 +114,7 @@ pub fn perform_udp(dest_addresses: Vec<u32>, socket: Arc<Socket>, mut rx_f: Rece
                 // bytes.extend_from_slice(&payload.destination_address.to_be_bytes()); // Bytes 16 - 19
                 // bytes.extend_from_slice(&payload.sender_client_id.to_be_bytes()); // Bytes 20 - 23
 
-                let bind_addr_dest = format!("{}:0", Ipv4Addr::from(dest_addr).to_string());
+                let bind_addr_dest = format!("{}:0", Ipv4Addr::from(dest_addr).to_string()); // TODO port
 
                 let udp = UDPPacket::udp_request(source_addr, dest_addr,source_port as u16, destination_port as u16, Vec::new()); // TODO what needs to be in the UDP body?
 
@@ -146,4 +146,63 @@ pub fn perform_udp(dest_addresses: Vec<u32>, socket: Arc<Socket>, mut rx_f: Rece
         }
     });
     println!("[Client outbound] UDP Outbound thread finished");
+}
+
+// Perform a TCP measurement/task
+pub fn perform_tcp(dest_addresses: Vec<u32>, socket: Arc<Socket>, mut rx_f: Receiver<()>, task_id: u32, client_id: u32, source_addr: u32, destination_port: u32, source_port: u32) {
+    println!("[Client outbound] Started TCP probing thread");
+    thread::spawn({
+        move || {
+            // Rate limiter
+            let mut lb = DirectRateLimiter::<LeakyBucket>::per_second(NonZeroU32::new(5000).unwrap());
+
+            // Loop over the destination addresses
+            for dest_addr in dest_addresses {
+
+
+                // TODO can I still store the transmit time information somehow?
+                // let transmit_time = SystemTime::now()
+                //     .duration_since(UNIX_EPOCH)
+                //     .unwrap()
+                //     .as_nanos() as u64;
+
+                // let mut bytes: Vec<u8> = Vec::new();
+                // bytes.extend_from_slice(&payload.task_id.to_be_bytes()); // Bytes 0 - 3
+                // bytes.extend_from_slice(&payload.transmit_time.to_be_bytes()); // Bytes 4 - 11
+                // bytes.extend_from_slice(&payload.source_address.to_be_bytes()); // Bytes 12 - 15
+                // bytes.extend_from_slice(&payload.destination_address.to_be_bytes()); // Bytes 16 - 19
+                // bytes.extend_from_slice(&payload.sender_client_id.to_be_bytes()); // Bytes 20 - 23
+
+                let bind_addr_dest = format!("{}:0", Ipv4Addr::from(dest_addr).to_string()); // TODO port
+
+                let tcp = TCPPacket::tcp_syn_ack(source_addr, dest_addr, source_port as u16, destination_port as u16, Vec::new()); // TODO what needs to be in the TCP body?
+
+                // Rate limiting
+                while let Err(_) = lb.check() {
+                    thread::sleep(Duration::from_millis(1));
+                }
+
+                // Send out packet
+                if let Err(e) = socket.send_to(
+                    &tcp,
+                    &bind_addr_dest
+                        .to_string()
+                        .parse::<SocketAddr>()
+                        .unwrap()
+                        .into(),
+                ) {
+                    error!("Failed to send TCP packet to socket: {:?}", e);
+                } else {
+                    // println!("[Client outbound] Packet sent!");
+                }
+            }
+            debug!("finished TCP probing");
+
+            // All packets have been sent for this task, give the listener 10 seconds for the replies
+            thread::sleep(Duration::from_secs(10));
+            // Now close down the listener
+            rx_f.close();
+        }
+    });
+    println!("[Client outbound] TCP Outbound thread finished");
 }
