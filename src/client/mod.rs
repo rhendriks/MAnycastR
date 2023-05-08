5a292240d46eaa8f25453c7d55b3b17cd1b5492e
rhendriks <remi.hendriks@utwente.nl>
2023-05-08 13:18:22 +0200

    Both source and destination ports for TCP are now unique for each client

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -185,7 +185,7 @@ impl Client {
             }
             Data::Tcp(_) => {
                 // Destination port is a high number to prevent causing open states on the target
-                let dest_port= 63853;
+                let dest_port = 63853 + client_id as u16;
                 let src_port = 62321 + client_id as u16;
                 // Create the socket to send and receive to/from
                 let bind_address = format!(
