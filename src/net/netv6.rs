3330eb98613634b8ba24800685168f9c8301fd26
rhendriks <remi.hendriks@utwente.nl>
2023-11-03 11:08:40 +0100

    Fixed type mismatch

--- src/net/netv6.rs
+++ src/net/netv6.rs
@@ -241,16 +241,17 @@ pub fn calculate_checksum_v6(buffer: &[u8], pseudo_header: &PseudoHeaderv6) -> u
     let packet_len = buffer.len();
     let mut sum = 0u32;
 
-    // Sum the pseudo header
-    sum += pseudo_header.source_address >> 48;
-    sum += (pseudo_header.source_address >> 32) & 0xffff;
-    sum += (pseudo_header.source_address >> 16) & 0xffff;
-    sum += pseudo_header.source_address & 0xffff;
+    // Sum the pseudo header source address (128 bits split into 4x 32 bits)
+    sum += (pseudo_header.source_address >> 96) as u32;
+    sum += ((pseudo_header.source_address >> 64) & 0xFFFF_FFFF) as u32;
+    sum += ((pseudo_header.source_address >> 32) & 0xFFFF_FFFF) as u32;
+    sum += (pseudo_header.source_address & 0xFFFF_FFFF) as u32;
 
-    sum += pseudo_header.destination_address >> 48;
-    sum += (pseudo_header.destination_address >> 32) & 0xffff;
-    sum += (pseudo_header.destination_address >> 16) & 0xffff;
-    sum += pseudo_header.destination_address & 0xffff;
+    // Sum the pseudo header destination address (128 bits split into 4x 32 bits)
+    sum += (pseudo_header.destination_address >> 96) as u32;
+    sum += ((pseudo_header.destination_address >> 64) & 0xFFFF_FFFF) as u32;
+    sum += ((pseudo_header.destination_address >> 32) & 0xFFFF_FFFF) as u32;
+    sum += (pseudo_header.destination_address & 0xFFFF_FFFF) as u32;
 
     sum += u32::from(pseudo_header.length);
     sum += u32::from(pseudo_header.next_header);
