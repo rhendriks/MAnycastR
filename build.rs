3d91f6efaa5443229f0abc725d0e24824067fa3c
Degen, Bernhard (UT-EEMCS) <b.j.degen@utwente.nl>
2024-01-22 14:57:40 +0100

    Ensure the latest commit is picked up

--- build.rs
+++ build.rs
@@ -9,4 +9,5 @@ fn main() {
     let output = Command::new("git").args(&["rev-parse", "--short=7", "HEAD"]).output().unwrap();
     let git_hash = String::from_utf8(output.stdout).unwrap();
     println!("cargo:rustc-env=GIT_HASH=git-{}", git_hash);
+    println!("cargo:rustc-rerun-if-changed=.git/HEAD");
 }
