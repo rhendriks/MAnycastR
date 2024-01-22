c537a731cc158dc61e429afc8b9b491a1f6f477f
rhendriks <remi.hendriks@utwente.nl>
2024-01-22 13:46:59 +0100

    removed printlns

--- src/client/outbound.rs
+++ src/client/outbound.rs
@@ -228,10 +228,6 @@ pub fn perform_udp(client_id: u8, source_address: IP, source_port: u16, mut outb
                     packet.extend_from_slice(&ethernet_header);
                     packet.extend_from_slice(&udp); // ip header included
 
-                    println!("UDP packet: ");
-                    for byte in &packet {
-                        print!("{:02x} ", byte);
-                    }
                     // Send out packet
                     cap.sendpacket(packet).expect("Failed to send UDP packet");
                 }
