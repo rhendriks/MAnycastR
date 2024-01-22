e2dc167836d76033c951303e3eb863d6c8c09bbd
Degen, Bernhard (UT-EEMCS) <b.j.degen@utwente.nl>
2024-01-22 14:38:25 +0100

    Include commit in version string

--- build.rs
+++ build.rs
@@ -1,5 +1,12 @@
+use std::process::Command;
+
 // Generates the tonic/grpc code from the .proto file
 fn main() {
     tonic_build::compile_protos("proto/verfploeter.proto")
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
+
+    // Gets commit string
+    let output = Command::new("git").args(&["rev-parse", "--short=7", "HEAD"]).output().unwrap();
+    let git_hash = String::from_utf8(output.stdout).unwrap();
+    println!("cargo:rustc-env=GIT_HASH=git-{}", git_hash);
 }
