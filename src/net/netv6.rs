4380aa11dc2f11e3e021e6d9a6ce3e84c1436ebb
Remi Hendriks <remi.hendriks@utwente.nl>
2024-01-22 13:13:36 +0100

    Debugging v6 checksum

--- src/net/netv6.rs
+++ src/net/netv6.rs
@@ -269,9 +269,12 @@ pub fn calculate_checksum_v6(mut buffer: Vec<u8>, pseudo_header: PseudoHeaderv6)
     // Sum the packet
     let mut i = 0;
     while i < packet_len - 1 { // TODO different than ipv4
-        let word = u16::from_be_bytes([packet[i], packet[i + 1]]);
-        sum += u32::from(word);
+        let mut rdr = Cursor::new(&buffer[i..]);
+        sum += u32::from(rdr.read_u16::<NetworkEndian>().unwrap());
         i += 2;
+        // let word = u16::from_be_bytes([packet[i], packet[i + 1]]);
+        // sum += u32::from(word);
+        // i += 2;
     }
 
     println!("Sum: {}", sum);
