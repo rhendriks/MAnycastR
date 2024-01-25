ca79ef2e1e33561628d02db25c37cfc7b47a494c
Remi Hendriks <remi.hendriks@utwente.nl>
2024-01-25 10:17:13 +0100

    Attempt at getting the correct v6 address for unicast measurements

--- src/client/mod.rs
+++ src/client/mod.rs
@@ -3,7 +3,7 @@ use custom_module::IP;
 use custom_module::verfploeter::{
     Finished, Task, Metadata, TaskResult, task::Data, ClientId, controller_client::ControllerClient, Address, Origin, End
 };
-use std::net::{Ipv4Addr, Ipv6Addr};
+use std::net::{IpAddr, Ipv4Addr, Ipv6Addr};
 use tonic::Request;
 use tonic::transport::Channel;
 use std::error::Error;
@@ -16,6 +16,7 @@ use futures::sync::oneshot;
 use crate::client::inbound::{listen_ping, listen_tcp, listen_udp};
 use crate::client::outbound::{perform_ping, perform_tcp, perform_udp};
 use local_ip_address::{local_ip, local_ipv6};
+use local_ip_address::list_afinet_netifas;
 
 mod inbound;
 mod outbound;
@@ -173,9 +174,16 @@ impl Client {
         // If this client has a specified source address use it, otherwise use the one from the task
         let source_addr: IP = if igreedy {
             let unicast_ip = if ipv6 {
-                IP::from(local_ipv6().expect("Unable to get local unicast IP address").to_string())
+                let ifas = list_afinet_netifas().expect("Unable to get local interfaces");
+                if let Some((_, ipaddr)) = ifas
+                    .iter()
+                    .find(|(name, ipaddr)| (*name == "enp1s0") && matches!(ipaddr, IpAddr::V6(_)) && (ipaddr.to_string() != "2001:610:9000::1")) {
+                    IP::from(ipaddr.to_string())
+                } else {
+                    panic!("Unable to find local unicast IPv6 address");
+                }
             } else {
-                IP::from(local_ip().expect("Unable to get local unicast IP address").to_string())
+                IP::from(local_ip().expect("Unable to get local unicast IPv4 address").to_string())
             };
 
             client_sources = vec![Origin {
