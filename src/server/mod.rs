85f1c3e014dea2263284b23b9549f13c987a8e7c
rhendriks <remi.hendriks@utwente.nl>
2024-01-29 15:01:26 +0100

    We now synchronously finish tasks

--- src/server/mod.rs
+++ src/server/mod.rs
@@ -642,13 +642,14 @@ impl Controller for ControllerService {
                 }
 
                 if !abort {
+                    println!("Trace route: {}", traceroute);
                     // Sleep 10 seconds to give the client time to finish the task and receive the last responses
                     if traceroute {
-                        tokio::time::sleep(Duration::from_secs(45)).await;
+                        tokio::time::sleep(Duration::from_secs((45 + clients.len() - client_id) as u64)).await;
                     } else {
-                        tokio::time::sleep(Duration::from_secs(10)).await;
+                        tokio::time::sleep(Duration::from_secs((10 + clients.len() - client_id) as u64)).await;
                     }
-                    println!("[Server] Letting client with ID {} know it has received all tasks", client_id);
+                    println!("[Server] Letting client with ID {} know it the measurement is finished", client_id);
                     // Send a message to the client to let it know it has received everything for the current task
                     match sender.send(Ok(Task {
                         data: None,
