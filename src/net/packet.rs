1b8ace54412fab1e069616b383457aa96eff217a
anycast-census <tangled@utwente.nl>
2025-03-06 14:38:36 +0100

    remove println statement

--- src/net/packet.rs
+++ src/net/packet.rs
@@ -73,8 +73,6 @@ pub fn get_ethernet_header(
             }
         }
     }
-
-    println!("using dst mac: {:?}", mac_dst);
     child.wait().expect("Failed to wait on child");
 
     // Construct the ethernet header
