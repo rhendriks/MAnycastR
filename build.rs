cf509f7ab8ad7b477019711039ce24fd3fd2bdb4
rhendriks <remi.hendriks@utwente.nl>
2025-03-18 12:59:50 +0100

    remove libpcap from dockerfile

--- build.rs
+++ build.rs
@@ -3,7 +3,7 @@ use std::process::Command;
 // Generates the tonic/grpc code from the .proto file
 fn main() {
     // Compile Protocol Buffers definitions
-    tonic_build::compile_protos("proto/verfploeter.proto")
+    tonic_build::compile_protos("proto/manycastr.proto")
         .unwrap_or_else(|e| panic!("Failed to compile protos {:?}", e));
 
     // Gets commit string
