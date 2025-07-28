5674238d64206bd2a3c3484a75e6c8f4e1749290
rhendriks <remi.hendriks@utwente.nl>
2025-07-28 10:44:21 +0200

    reverse bytes order

--- src/net/packet.rs
+++ src/net/packet.rs
@@ -28,10 +28,11 @@ fn get_default_gateway_ip_linux() -> Result<String, String> {
             let bytes: Vec<u8> = (0..4)
                 .map(|i| u8::from_str_radix(&hex[2 * i..2 * i + 2], 16).unwrap())
                 .collect();
-
+            
+            // Return bytes in reverse order (little-endian format)
             return Ok(format!(
                 "{}.{}.{}.{}",
-                bytes[0], bytes[1], bytes[2], bytes[3]
+                bytes[3], bytes[2], bytes[1], bytes[0]
             ));
         }
     }
