92aebf913e7c978d93be435e2ba08382a2bf434e
rhendriks <remi.hendriks@utwente.nl>
2025-07-29 10:43:21 +0200

    debug

--- src/orchestrator/mod.rs
+++ src/orchestrator/mod.rs
@@ -959,6 +959,7 @@ impl Controller for ControllerService {
 
         // if self.r_prober is not None and equals this task's worker_id
         if self.is_responsive.load(std::sync::atomic::Ordering::SeqCst) {
+            println!("received {:?}", task_result);
             // Get the list of targets
             let responsive_targets: Vec<Address> = task_result
                 .result_list
