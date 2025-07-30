920790b2e3261d82f60ed358a1e128d83bcfb1dd
remi <remi.hendriks@utwente.nl>
2025-07-30 13:23:03 +0200

    split path checker'

--- src/cli/mod.rs
+++ src/cli/mod.rs
@@ -447,61 +447,8 @@ pub async fn execute(args: &ArgMatches) -> Result<(), Box<dyn Error>> {
 
         // get optional path to write results to
         let path = matches.get_one::<String>("out");
-        if let Some(path_str) = path {
-            let path = Path::new(path_str);
-
-            if !path_str.ends_with('/') {
-                // User provided a file
-                if path.exists() {
-                    if path.is_dir() {
-                        println!("[CLI] Path is already a directory, exiting");
-                        return Err("Path is already a directory".into());
-                    } else if fs::metadata(path)
-                        .expect("Unable to get path metadata")
-                        .permissions()
-                        .readonly()
-                    {
-                        println!("[CLI] Lacking write permissions for file {}", path_str);
-                        return Err("Lacking write permissions".into());
-                    } else {
-                        println!(
-                            "[CLI] Overwriting existing file {} when measurement is done",
-                            path_str
-                        );
-                    }
-                } else {
-                    println!("[CLI] Writing results to new file {}", path_str);
-
-                    // File does not yet exist, create it to verify permissions
-                    File::create(path)
-                        .expect("Unable to create output file")
-                        .sync_all()
-                        .expect("Unable to sync file");
-                    fs::remove_file(path).expect("Unable to remove file");
-                }
-            } else {
-                // User provided a directory
-                if path.exists() {
-                    if !path.is_dir() {
-                        println!("[CLI] Path is already a file, exiting");
-                        return Err("Cannot make dir, file with name already exists.".into());
-                    } else if fs::metadata(path)
-                        .expect("Unable to get path metadata")
-                        .permissions()
-                        .readonly()
-                    {
-                        println!("[CLI] Lacking write permissions for directory {}", path_str);
-                        return Err("Path is not writable".into());
-                    } else {
-                        println!("[CLI] Writing results to existing directory {}", path_str);
-                    }
-                } else {
-                    println!("[CLI] Writing results to new directory {}", path_str);
-
-                    // Attempt creating path to verify permissions
-                    fs::create_dir_all(path).expect("Unable to create output directory");
-                }
-            }
+        if let Some(value) = validate_path_perms(path) {
+            return value;
         }
 
         // Create the measurement definition and send it to the orchestrator
@@ -541,6 +488,66 @@ pub async fn execute(args: &ArgMatches) -> Result<(), Box<dyn Error>> {
     } else {
         panic!("Unrecognized command");
     }
+}
+
+fn validate_path_perms(path: Option<&String>) -> Option<Result<(), Box<dyn Error>>> {
+    if let Some(path_str) = path {
+        let path = Path::new(path_str);
+
+        if !path_str.ends_with('/') {
+            // User provided a file
+            if path.exists() {
+                if path.is_dir() {
+                    println!("[CLI] Path is already a directory, exiting");
+                    return Some(Err("Path is already a directory".into()));
+                } else if fs::metadata(path)
+                    .expect("Unable to get path metadata")
+                    .permissions()
+                    .readonly()
+                {
+                    println!("[CLI] Lacking write permissions for file {}", path_str);
+                    return Some(Err("Lacking write permissions".into()));
+                } else {
+                    println!(
+                        "[CLI] Overwriting existing file {} when measurement is done",
+                        path_str
+                    );
+                }
+            } else {
+                println!("[CLI] Writing results to new file {}", path_str);
+
+                // File does not yet exist, create it to verify permissions
+                File::create(path)
+                    .expect("Unable to create output file")
+                    .sync_all()
+                    .expect("Unable to sync file");
+                fs::remove_file(path).expect("Unable to remove file");
+            }
+        } else {
+            // User provided a directory
+            if path.exists() {
+                if !path.is_dir() {
+                    println!("[CLI] Path is already a file, exiting");
+                    return Some(Err("Cannot make dir, file with name already exists.".into()));
+                } else if fs::metadata(path)
+                    .expect("Unable to get path metadata")
+                    .permissions()
+                    .readonly()
+                {
+                    println!("[CLI] Lacking write permissions for directory {}", path_str);
+                    return Some(Err("Path is not writable".into()));
+                } else {
+                    println!("[CLI] Writing results to existing directory {}", path_str);
+                }
+            } else {
+                println!("[CLI] Writing results to new directory {}", path_str);
+
+                // Attempt creating path to verify permissions
+                fs::create_dir_all(path).expect("Unable to create output directory");
+            }
+        }
+    }
+    None
 }
 
 /// Get the hitlist from a file.
