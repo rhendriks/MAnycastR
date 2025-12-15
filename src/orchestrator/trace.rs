575d16a0132bc5195c476dc53db9c8fd55da3308
rhendriks <remi.hendriks@utwente.nl>
2025-12-15 16:09:13 +0100

    check timeouts

--- src/orchestrator/trace.rs
+++ src/orchestrator/trace.rs
@@ -1,6 +1,8 @@
 use crate::custom_module::manycastr::{task, Address, Task, Trace};
 use std::collections::{HashMap, VecDeque};
 use std::sync::{Arc, Mutex};
+use std::sync::atomic::{AtomicBool, Ordering};
+use std::thread;
 use std::time::{Duration, Instant};
 
 /// Traceroute parameters
@@ -56,86 +58,104 @@ pub struct TraceIdentifier {
 /// # Arguments
 /// * 'worker_stacks' - Shared stack to put Trace tasks in for workers.
 /// * 'sessions' - Shared map that tracks ongoing sessions.
+/// * 'active_workers' - Break signal that is set to None when measurement is finished
 pub fn check_trace_timeouts(
     worker_stacks: Arc<Mutex<HashMap<u32, VecDeque<Task>>>>,
     session_tracker: Arc<Mutex<SessionTracker>>,
+    active_workers:  Arc<Mutex<Option<u32>>>,
 ) {
-    // Keep track of tasks to send to the workers
-    let mut tasks_to_send = Vec::new();
-    let now = Instant::now();
+    loop {
+        println!("checking trace timeouts");
 
-    // TODO needs to be looped
-    {
-        // Lock tracker
-        let mut session_tracker = session_tracker.lock().unwrap();
-
-        // Iteratively check top of the stack (oldest sessions) to see if they timed out
-        while let Some((_id, deadline)) = session_tracker.expiration_queue.front() {
-            if *deadline > now {
-                // Deadline is in the future
-                break; // release lock and exit
+        // Check if we are finished
+        {
+            let worker_guard = active_workers.lock().unwrap();
+            if worker_guard.is_none() {
+                break;
             }
-            // Pop candidate
-            let (id, _old_deadline) = session_tracker.expiration_queue.pop_front().unwrap();
+        }
 
-            // Get session belonging to identifier
-            let should_recycle = if let Some(session) = session_tracker.sessions.get_mut(&id) {
-                // Verify the session is still timed out (might have been updated)
-                let expiration = session.last_updated + HOP_TIMEOUT;
+        // Keep track of tasks to send to the workers
+        let mut tasks_to_send = Vec::new();
+        let now = Instant::now();
 
-                if expiration > now {
-                    // Still alive (received update during check) -> update deadline
-                    Some((id.clone(), expiration))
-                } else {
-                    // No longer alive
-                    session.consecutive_failures += 1;
-                    session.last_updated = now;
-                    session.current_ttl += 1;
+        {
+            // Lock tracker
+            let mut session_tracker = session_tracker.lock().unwrap();
 
-                    // Check termination conditions
-                    if session.consecutive_failures >= MAX_CONSECUTIVE_FAILURES
-                        || session.current_ttl > MAX_TTL
-                    {
-                        // Remove from tracker
-                        session_tracker.sessions.remove(&id);
-                        None // Nothing to update
-                    } else {
-                        // Measure the next hop (current hop timed out)
-
-                        println!("Hop timed out performing follow-up trace from {} to {:?} with TTL {}", session.worker_id, session.target, session.current_ttl);
-
-                        tasks_to_send.push((
-                            session.worker_id,
-                            Task {
-                                task_type: Some(task::TaskType::Trace(Trace {
-                                    dst: session.target,
-                                    ttl: session.current_ttl as u32,
-                                    origin_id: session.origin_id,
-                                })),
-                            },
-                        ));
-
-                        // Update deadline for current session
-                        Some((id.clone(), now + HOP_TIMEOUT))
-                    }
+            // Iteratively check top of the stack (oldest sessions) to see if they timed out
+            while let Some((_id, deadline)) = session_tracker.expiration_queue.front() {
+                if *deadline > now {
+                    // Deadline is in the future
+                    break; // release lock and exit
                 }
-            } else {
-                // Session removed during process (drop from tracker)
-                None
-            };
+                // Pop candidate
+                let (id, _old_deadline) = session_tracker.expiration_queue.pop_front().unwrap();
 
-            // If we need to keep tracking the current session, put it in the end of the queue
-            if let Some(item) = should_recycle {
-                session_tracker.expiration_queue.push_back(item);
+                // Get session belonging to identifier
+                let should_recycle = if let Some(session) = session_tracker.sessions.get_mut(&id) {
+                    // Verify the session is still timed out (might have been updated)
+                    let expiration = session.last_updated + HOP_TIMEOUT;
+
+                    if expiration > now {
+                        // Still alive (received update during check) -> update deadline
+                        Some((id.clone(), expiration))
+                    } else {
+                        // No longer alive
+                        session.consecutive_failures += 1;
+                        session.last_updated = now;
+                        session.current_ttl += 1;
+
+                        // Check termination conditions
+                        if session.consecutive_failures >= MAX_CONSECUTIVE_FAILURES
+                            || session.current_ttl > MAX_TTL
+                        {
+                            // Remove from tracker
+                            session_tracker.sessions.remove(&id);
+                            None // Nothing to update
+                        } else {
+                            // Measure the next hop (current hop timed out)
+
+                            println!("Hop timed out performing follow-up trace from {} to {} with TTL {}", session.worker_id, session.target.unwrap(), session.current_ttl);
+
+                            tasks_to_send.push((
+                                session.worker_id,
+                                Task {
+                                    task_type: Some(task::TaskType::Trace(Trace {
+                                        dst: session.target,
+                                        ttl: session.current_ttl as u32,
+                                        origin_id: session.origin_id,
+                                    })),
+                                },
+                            ));
+
+                            // Update deadline for current session
+                            Some((id.clone(), now + HOP_TIMEOUT))
+                        }
+                    }
+                } else {
+                    // Session removed during process (drop from tracker)
+                    None
+                };
+
+                // If we need to keep tracking the current session, put it in the end of the queue
+                if let Some(item) = should_recycle {
+                    session_tracker.expiration_queue.push_back(item);
+                }
             }
         }
+
+        // Put tasks in worker stacks
+        if !tasks_to_send.is_empty() {
+            let mut stacks = worker_stacks.lock().unwrap();
+            for (worker_id, task_to_send) in tasks_to_send {
+                stacks.entry(worker_id).or_default().push_back(task_to_send);
+            }
+        }
+
+        // Sleep for the timeout interval before checking timeouts again
+        thread::sleep(HOP_TIMEOUT);
     }
 
-    // Put tasks in worker stacks
-    if !tasks_to_send.is_empty() {
-        let mut stacks = worker_stacks.lock().unwrap();
-        for (worker_id, task_to_send) in tasks_to_send {
-            stacks.entry(worker_id).or_default().push_back(task_to_send);
-        }
-    }
+    println!("[Orchestrator] Finished trace timeout thread")
 }
