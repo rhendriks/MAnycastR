8e66c5d6a478a739364ed2f0190f380eb96bff77
anycast <remi.hendriks@utwente.nl>
2025-12-19 16:37:06 +0100

    split outbound

---
+++ src/worker/outbound/mod.rs
@@ -0,0 +1,186 @@
+mod trace;
+mod record_route;
+mod probe;
+
+use log::{error, info, warn};
+use std::num::NonZeroU32;
+use std::sync::atomic::AtomicBool;
+use std::sync::Arc;
+use std::thread;
+use std::thread::sleep;
+use std::time::{Duration, SystemTime, UNIX_EPOCH};
+use tokio::sync::mpsc::{error::TryRecvError, Receiver};
+
+use crate::custom_module;
+use custom_module::manycastr::Origin;
+
+use pnet::datalink::DataLinkSender;
+
+use crate::custom_module::manycastr::instruction::InstructionType;
+use crate::custom_module::manycastr::task::TaskType;
+use crate::custom_module::manycastr::{Address, Trace};
+use crate::custom_module::Separated;
+use crate::net::packet::{
+    create_dns, create_icmp, create_record_route_icmp, create_tcp, get_ethernet_header,
+    ProbePayload,
+};
+use ratelimit_meter::{DirectRateLimiter, LeakyBucket};
+use crate::worker::outbound::probe::send_probe;
+use crate::worker::outbound::record_route::send_record_route_probe;
+use crate::worker::outbound::trace::send_trace;
+
+const DISCOVERY_WORKER_ID_OFFSET: u32 = u16::MAX as u32;
+
+/// Configuration for an outbound packet sending worker.
+///
+/// This struct holds all the parameters needed to initialize and run a worker
+/// that generates and sends measurement probes (e.g., ICMP, DNS, TCP)
+/// at a specified rate.
+pub struct OutboundConfig {
+    /// The unique ID of this specific worker.
+    pub worker_id: u16,
+    /// A list of source addresses and port values (`Origin`) to send probes from.
+    pub tx_origins: Vec<Origin>,
+    /// A shared signal that can be used to forcefully shut down the worker.
+    ///
+    /// E.g., when the CLI has abruptly disconnected.
+    pub abort_s: Arc<AtomicBool>,
+    /// Indicates if this is a latency measurement.
+    pub is_latency: bool,
+    /// The unique ID of the measurement.
+    pub m_id: u32,
+    /// The type of probe to send (e.g., 1 for ICMP, 2 for DNS/A, 3 for TCP).
+    pub m_type: u8,
+    /// The domain name to query in DNS measurement probes.
+    pub qname: String,
+    /// An informational URL to be embedded in the probe's payload (e.g., an opt-out link).
+    pub info_url: String,
+    /// The name of the network interface to send packets from (e.g., "eth0").
+    pub if_name: String,
+    /// The target rate for sending probes, measured in packets per second (pps).
+    pub probing_rate: u32,
+    /// Vector of origins to find the matching origin ID for traceroute tasks
+    pub origin_map: Option<Vec<Origin>>,
+    /// Indicates if the measurement is IPv6 (true) or IPv4 (false).
+    pub is_ipv6: bool,
+}
+
+/// Starts the outbound worker thread that awaits tasks and sends probes.
+///
+/// # Arguments
+///
+/// * 'config' - configuration for the outbound worker thread
+///
+/// * 'outbound_rx' - on this channel we receive future tasks that are part of the current measurement
+///
+/// * 'socket_tx' - the sender object to send packets
+pub fn outbound(
+    config: OutboundConfig,
+    mut outbound_rx: Receiver<InstructionType>,
+    mut socket_tx: Box<dyn DataLinkSender>,
+) {
+    thread::Builder::new()
+        .name("outbound".to_string())
+        .spawn(move || {
+            let mut sent: u32 = 0;
+            let mut sent_discovery = 0;
+            let mut traces_sent: u32 = 0;
+            let mut failed: u32 = 0;
+            // Rate limit the number of packets sent per second, each origin has the same rate (i.e., sending with 2 origins will double the rate)
+            let mut limiter = DirectRateLimiter::<LeakyBucket>::per_second(NonZeroU32::new(config.probing_rate * config.tx_origins.len() as u32).unwrap());
+
+            let ethernet_header = get_ethernet_header(config.is_ipv6, &config.if_name);
+            'outer: loop {
+                if config.abort_s.load(std::sync::atomic::Ordering::SeqCst) {
+                    // If the finish_rx is set to true, break the loop (abort)
+                    warn!("[Worker outbound] ABORTING");
+                    break;
+                }
+                let instruction;
+                // Receive tasks from the outbound channel
+                loop {
+                    match outbound_rx.try_recv() {
+                        Ok(t) => {
+                            instruction = t;
+                            break;
+                        }
+                        Err(e) => {
+                            if e == TryRecvError::Disconnected {
+                                info!("[Worker outbound] Channel disconnected");
+                                break 'outer;
+                            }
+                            // wait some time and try again
+                            sleep(Duration::from_millis(100));
+                        }
+                    };
+                }
+                match instruction {
+                    InstructionType::End(_) => {
+                        // An End task means the measurement has finished
+                        break;
+                    }
+                    InstructionType::Tasks(tasks) => {
+                        for task in tasks.tasks.iter() {
+                            match &task.task_type {
+                                Some(TaskType::Probe(task)) => {
+                                    let (s, f) = send_probe(
+                                        &config,
+                                        &ethernet_header,
+                                        &task.dst.unwrap(),
+                                        &mut socket_tx,
+                                        &mut limiter,
+                                        false, // Not a discovery probe
+                                    );
+                                    sent += s;
+                                    failed += f;
+                                },
+                                Some(TaskType::Discovery(task)) => {
+                                    let (s, f) = send_probe(
+                                        &config,
+                                        &ethernet_header,
+                                        &task.dst.unwrap(),
+                                        &mut socket_tx,
+                                        &mut limiter,
+                                        true, // This is a discovery probe
+                                    );
+                                    sent_discovery += s;
+                                    failed += f;
+                                },
+                                Some(TaskType::Trace(trace)) => {
+                                    let (s, f) = send_trace(
+                                        &ethernet_header,
+                                        config.worker_id as u32,
+                                        config.m_id,
+                                        &config.info_url,
+                                        trace,
+                                        &mut socket_tx,
+                                        config.origin_map.as_ref().expect("Missing origin_map"),
+                                    );
+                                    traces_sent += s;
+                                    failed += f;
+                                }
+                                Some(TaskType::Record(task)) => {
+                                    let (s, f) = send_record_route_probe(
+                                        &ethernet_header,
+                                        &config,
+                                        &task.dst.unwrap(),
+                                        &mut socket_tx,
+                                        &mut limiter,
+                                    );
+                                    sent += s;
+                                    failed += f;
+                                }
+                                _ => continue, // Invalid task type
+                            };
+                        }
+                    }
+                    _ => continue, // Invalid measurement
+                };
+            }
+            info!("[Worker outbound] Outbound thread finished - packets sent : {} (including {} discovery probes), packets failed to send: {}", sent.with_separator(), sent_discovery.with_separator(), failed.with_separator());
+            if traces_sent > 0 {
+                info!("[Worker outbound] Traceroute probes sent: {}", traces_sent.with_separator());
+            }
+        })
+        .expect("Failed to spawn outbound thread");
+}
---
+++ src/worker/outbound/probe.rs
@@ -0,0 +1,99 @@
+use std::thread::sleep;
+use std::time::Duration;
+use log::{error, warn};
+use pnet::datalink::DataLinkSender;
+use ratelimit_meter::{DirectRateLimiter, LeakyBucket};
+use crate::custom_module::manycastr::Address;
+use crate::net::packet::{create_dns, create_icmp, create_tcp, ProbePayload};
+use crate::worker::outbound::{OutboundConfig, DISCOVERY_WORKER_ID_OFFSET};
+
+/// Sends probes to the specified destination using the provided measurement configuration.
+/// This function constructs the appropriate packet based on the measurement type
+/// and sends it through the provided socket.
+/// # Arguments
+///
+/// * 'config' - The outbound configuration containing worker details and settings.
+/// * 'ethernet_header' - The Ethernet header to prepend to the packet.
+/// * 'dst' - The destination address to which the probes will be sent.
+/// * 'socket_tx' - The socket sender to use for sending the packet.
+/// * 'limiter' - A rate limiter to control the sending rate of packets.
+/// * 'is_discovery' - A boolean indicating whether the probes are for discovery purposes.
+/// # Returns
+/// A tuple containing the number of successfully sent packets and the number of failed sends.
+pub fn send_probe(
+    config: &OutboundConfig,
+    ethernet_header: &[u8],
+    dst: &Address,
+    socket_tx: &mut Box<dyn DataLinkSender>,
+    limiter: &mut DirectRateLimiter<LeakyBucket>,
+    is_discovery: bool, // Whether we are sending a discovery probe
+) -> (u32, u32) {
+    let origins = &config.tx_origins;
+    let worker_id = if is_discovery {
+        config.worker_id as u32 + DISCOVERY_WORKER_ID_OFFSET // Use a different worker ID range for discovery probes
+    } else {
+        config.worker_id as u32
+    };
+    let m_type = config.m_type;
+    let is_symmetric = config.is_latency; // For TCP, use the is_latency flag
+    let m_id = config.m_id;
+    let info_url = &config.info_url;
+    let mut sent = 0;
+    let mut failed = 0;
+    let qname = &config.qname;
+    for origin in origins {
+        let mut packet = ethernet_header.to_owned();
+        match m_type {
+            1 => {
+                let payload_fields = ProbePayload {
+                    worker_id,
+                    m_id,
+                    info_url,
+                };
+                // ICMP
+                packet.extend_from_slice(&create_icmp(
+                    &origin.src.unwrap(),
+                    dst,
+                    origin.dport as u16, // ICMP identifier
+                    2,                   // ICMP sequence number
+                    payload_fields,
+                    255,
+                ));
+            }
+            2 | 4 => {
+                // DNS A record or CHAOS
+                packet.extend_from_slice(&create_dns(origin, dst, worker_id, m_type, qname));
+            }
+            3 => {
+                // TCP
+                packet.extend_from_slice(&create_tcp(
+                    origin,
+                    dst,
+                    worker_id,
+                    is_symmetric, // is_symmetric
+                    info_url,
+                ));
+            }
+            255 => {
+                panic!("Invalid measurement type)") // TODO all, any, tracemap
+            }
+            _ => panic!("Invalid measurement type"), // Invalid measurement
+        }
+
+        while limiter.check().is_err() {
+            // Rate limit to avoid bursts
+            sleep(Duration::from_millis(1));
+        }
+
+        match socket_tx.send_to(&packet, None) {
+            Some(Ok(())) => sent += 1,
+            Some(Err(e)) => {
+                warn!("[Worker outbound] Failed to send ICMP packet: {e}");
+                failed += 1;
+            }
+            None => error!("[Worker outbound] Failed to send packet: No Tx interface"),
+        }
+    }
+
+    (sent, failed)
+}
