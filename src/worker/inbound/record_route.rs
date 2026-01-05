854ac0e60d6b1eaf06fccd60f71b0b9b6ec05c66
rhendriks <remi.hendriks@utwente.nl>
2026-01-05 10:35:42 +0100

    simplify record route parsing

--- src/worker/inbound/record_route.rs
+++ src/worker/inbound/record_route.rs
@@ -1,8 +1,6 @@
-use crate::custom_module::manycastr::reply::ReplyData;
-use crate::custom_module::manycastr::{Address, Origin, MeasurementReply, Reply};
-use crate::net::{parse_record_route_option, IPv4Packet, PacketPayload};
-use crate::worker::config::get_origin_id;
-use std::time::{SystemTime, UNIX_EPOCH};
+use crate::custom_module::manycastr::{Origin, Reply};
+use crate::net::{parse_record_route_option, IPPacket, IPv4Packet, PacketPayload};
+use crate::worker::inbound::ping::parse_icmp_inner;
 
 /// Parse ICMP Record Route packets (including v4/v6 headers) into a Reply result with trace information.
 ///
@@ -31,54 +29,21 @@ pub fn parse_record_route(
     }
 
     // Parse IP header
-    let ip_header = IPv4Packet::from(packet_bytes);
+    let ipv4_header = IPv4Packet::from(packet_bytes);
 
     // Get options (if any)
-    let recorded_hops = if let Some(ip_option) = ip_header.options {
-        parse_record_route_option(&ip_option)
+    let recorded_hops = if let Some(ip_option) = &ipv4_header.options {
+        parse_record_route_option(ip_option)
     } else {
         return None; // No options, cannot be a RR packet
     };
-
+    
+    let ip_header = IPPacket::V4(ipv4_header);
+    
     // Parse ICMP header
-    let PacketPayload::Icmp { value: icmp_packet } = ip_header.payload else {
+    let PacketPayload::Icmp { value: icmp_packet } = ip_header.payload() else {
         return None;
     };
-
-    // Make sure that this packet belongs to this measurement
-    let pkt_measurement_id: [u8; 4] = icmp_packet.payload[0..4].try_into().ok()?; // TODO move to initial if statement
-    if u32::from_be_bytes(pkt_measurement_id) != m_id {
-        return None;
-    }
-
-    let tx_time = u64::from_be_bytes(icmp_packet.payload[4..12].try_into().unwrap());
-    let tx_id = u32::from_be_bytes(icmp_packet.payload[12..16].try_into().unwrap());
-    let probe_src = Address::from(u32::from_be_bytes(
-        icmp_packet.payload[16..20].try_into().unwrap(),
-    ));
-    let probe_dst = Address::from(u32::from_be_bytes(
-        icmp_packet.payload[20..24].try_into().unwrap(),
-    ));
-    if (probe_src.get_v4() != ip_header.dst) || (probe_dst.get_v4() != ip_header.src) {
-        return None; // spoofed reply
-    }
-
-    let origin_id = get_origin_id(Address::from(ip_header.dst), 0, 0, worker_map)?;
-    let rx_time = SystemTime::now()
-        .duration_since(UNIX_EPOCH)
-        .unwrap()
-        .as_micros() as u64;
-
-    Some(Reply {
-        reply_data: Some(ReplyData::Measurement(MeasurementReply {
-            src: Some(Address::from(ip_header.src)),
-            ttl: ip_header.ttl as u32,
-            origin_id,
-            rx_time,
-            tx_time,
-            tx_id,
-            chaos: None,
-            recorded_hops,
-        })),
-    })
+    
+    parse_icmp_inner(&icmp_packet, &ip_header, m_id, worker_map, is_ipv6, recorded_hops)
 }
