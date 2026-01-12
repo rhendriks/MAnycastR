62a42e510dbe4bc1634c161c340b3231e7c41b10
rhendriks <remi.hendriks@utwente.nl>
2026-01-12 17:01:57 +0100

    formatting and clippy

--- src/worker/inbound/trace.rs
+++ src/worker/inbound/trace.rs
@@ -23,13 +23,12 @@ use std::time::{SystemTime, UNIX_EPOCH};
 pub fn parse_trace(
     packet_bytes: &[u8],
     m_id: u32,
-    is_ipv6: bool,
     src: Address,
     ttl: u32,
     origin_id: u32,
 ) -> Option<Reply> {
     // Check for ICMP Time Exceeded code
-    let (min_len, type_idx, expected_type) = if is_ipv6 {
+    let (min_len, type_idx, expected_type) = if src.is_v6() {
         (48, 0, 3) // IPv6: Min length 48, ICMP type at index 0, Type 3
     } else {
         (56, 20, 11) // IPv4: Min length 56, ICMP type at index 20, Type 11
@@ -37,23 +36,23 @@ pub fn parse_trace(
 
     if packet_bytes.len() < min_len || packet_bytes[type_idx] != expected_type {
         // Not ICMP Time exceeded; try to parse as ICMP echo reply from the target
-        return parse_icmp(packet_bytes, m_id, is_ipv6, true, src, origin_id, ttl);
+        return parse_icmp(packet_bytes, m_id, true, src, origin_id, ttl);
     }
 
-    let ip_header = if is_ipv6 {
+    let ip_header = if src.is_v6() {
         IPPacket::V6(IPv6Packet::from(packet_bytes))
     } else {
         IPPacket::V4(IPv4Packet::from(packet_bytes))
     };
 
-    let icmp_packet = if is_ipv6 {
+    let icmp_packet = if src.is_v6() {
         ICMPPacket::from(packet_bytes) // no IP header
     } else {
         ICMPPacket::from(&packet_bytes[20..]) // skip IPv4 header
     };
 
     // Parse IP header that caused the Time Exceeded (first 20 bytes of the ICMP body)
-    let original_ip_header = if is_ipv6 {
+    let original_ip_header = if src.is_v6() {
         IPPacket::V6(IPv6Packet::from(icmp_packet.payload.as_bytes()))
     } else {
         IPPacket::V4(IPv4Packet::from(icmp_packet.payload.as_bytes()))
