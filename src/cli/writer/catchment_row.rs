c290ed06bc09eb6201b24cb7f25732fc0d9b0e74
rhendriks <remi.hendriks@utwente.nl>
2026-01-26 11:49:37 +0100

    rename verfploeter to catchment in doc

--- src/cli/writer/catchment_row.rs
+++ src/cli/writer/catchment_row.rs
@@ -8,11 +8,11 @@ use bimap::BiHashMap;
 /// * `result` - The Reply that is being written to this row
 /// * `rx_id` - The worker ID of the receiver
 /// * `worker_map` - A map of worker IDs to hostnames, used to convert worker IDs to hostnames in the results
+/// * `origin_id` - Origin ID associated with the reply (0 if a single origin is used)
 ///
 /// # Returns
-///
 /// A vector of strings representing the row in the CSV file
-pub fn get_verfploeter_csv_row(
+pub fn get_catchment_csv_row(
     reply: MeasurementReply,
     rx_id: &u32,
     worker_map: &BiHashMap<u32, String>,
