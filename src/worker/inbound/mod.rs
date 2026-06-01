76d8f94195950dffd7349ca9ad3c9bbef9f7444f
remi <remi.hendriks@utwente.nl>
2026-06-01 14:10:35 +0200

    TODO

--- src/worker/inbound/mod.rs
+++ src/worker/inbound/mod.rs
@@ -251,7 +251,7 @@ fn handle_results(
     loop {
         sleep(Duration::from_secs(1));
 
-        let mut rq = Vec::new();
+        let mut rq = Vec::new(); // TODO assess using a capacity for each replybatch
         while let Ok(reply) = reply_rx.try_recv() {
             rq.push(reply);
         }
