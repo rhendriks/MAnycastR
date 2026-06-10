ffc9ce31ae2352fa7e5c18797727b0184ced368d
remi <remi.hendriks@utwente.nl>
2026-06-10 15:03:36 +0200

    new workerstatus enum

--- src/orchestrator/task_distributor.rs
+++ src/orchestrator/task_distributor.rs
@@ -2,7 +2,7 @@ use crate::custom_module::manycastr::{
     instruction, task, Address, End, Instruction, Probe, Task, Tasks,
 };
 use crate::orchestrator::worker::WorkerSender;
-use crate::orchestrator::worker::WorkerStatus::Probing;
+use crate::custom_module::manycastr::WorkerStatus::Probing;
 use crate::orchestrator::MeasurementHandle;
 use crate::ALL_WORKERS;
 use log::{info, warn};
