205b794e197cdcc7de8bcbe0139ab226b09dd1f5
remi <remi.hendriks@utwente.nl>
2026-08-13 12:27:43 +0200

    use tlsoptions

--- src/cli/commands/mod.rs
+++ src/cli/commands/mod.rs
@@ -1,5 +1,6 @@
 use crate::cli::client::CliClient;
 use crate::custom_module::manycastr::Empty;
+use crate::tls::TlsOptions;
 use bimap::BiHashMap;
 use clap::ArgMatches;
 use log::info;
@@ -16,12 +17,11 @@ mod worker_list;
 #[tokio::main]
 pub async fn execute(args: &ArgMatches) -> Result<(), Box<dyn Error>> {
     let server_address = args.get_one::<String>("orchestrator").unwrap();
-    let cert_path = args.get_one::<String>("tls").map(String::as_str);
-    let tls_domain = args.get_one::<String>("tls_domain").map(String::as_str);
+    let tls = TlsOptions::from_args(args);
 
     // Connect with orchestrator
     info!("[CLI] Connecting to orchestrator - {server_address}");
-    let mut grpc_client = CliClient::connect(server_address, cert_path, tls_domain)
+    let mut grpc_client = CliClient::connect(server_address, &tls)
         .await
         .map_err(|e| format!("Unable to connect to orchestrator: {e}"))?
         .send_compressed(CompressionEncoding::Zstd);
