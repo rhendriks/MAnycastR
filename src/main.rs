2a1f3d4c8669f8bcc55514ff137ec1a8e3752abb
remi <remi.hendriks@utwente.nl>
2026-08-13 14:15:33 +0200

    split cli for clap-markdown generation, and use lowercase manycastr

--- src/main.rs
+++ src/main.rs
@@ -130,7 +130,11 @@ fn main() {
 
 /// Parse command line arguments using clap
 fn parse_cmd() -> ArgMatches {
-    Command::new("MAnycastR")
+    cli().get_matches()
+}
+
+fn cli() -> Command {
+    Command::new("manycastr")
         .version(env!("GIT_HASH"))
         .author("Remi Hendriks <remi.hendriks@utwente.nl>")
         .about("Performs synchronized Internet measurement from a distributed set of anycast Points of Presence (PoPs)")
@@ -234,5 +238,4 @@ fn parse_cmd() -> ArgMatches {
                     .arg(arg!(-u --url <URL> "URL encoded in probe payload (e.g., opt-out URL)"))
                 )
             )
-        .get_matches()
 }
